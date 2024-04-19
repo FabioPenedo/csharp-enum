f3cd681f11d0653707db06a21d226c297052513c
FabioPenedo <fabiopenedo21@gmail.com>
2024-04-19 14:31:17 -0300

    primeiro commit, enum

---
+++ App/Entities/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using App.Entities.Enums;
+
+namespace App.Entities
+{
+    internal class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Moment
+                + ", "
+                + Status;
+        }
+    }
+}
---
+++ App/Program.cs
@@ -0,0 +1,33 @@
+﻿using App.Entities;
+using App.Entities.Enums;
+using System;
+
+namespace App // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new()
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+            Console.WriteLine(order);
+
+            OrderStatus os1 = (OrderStatus)2;
+            Console.WriteLine(os1);
+
+            int n1 = (int)OrderStatus.Processing;
+            Console.WriteLine(n1);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+            Console.WriteLine(txt);
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(os);
+            
+        }
+    }
+}
